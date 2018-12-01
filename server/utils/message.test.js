f975ee8d885f5dc23fc960fb904b7207dcbcd56d
Manish Arora <manish.arora1392@gmail.com>
2018-11-30 23:35:43 -0800

    Unit testing expect (version corrected)

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -4,11 +4,11 @@ var {generateMessage} = require('./message');
 
 describe('generateMessage',()=>{
     it('should generate correct message object',()=>{
-        // var from =  'Jen';
-        // var text = 'Some Message';
-        // var message = generateMessage(from,text);
+        var from =  'Jen';
+        var text = 'Some Message';
+        var message = generateMessage(from,text);
 
-        // expect(message.createdAt).toBe('number');
-        //expect(message).toInclude({from,text});
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from,text});
     });
 });
