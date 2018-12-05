6d6187c233327430939964cbf3f3aaff321e6544
Manish Arora <manish.arora1392@gmail.com>
2018-12-05 10:46:25 -0800

    Add User class with methods and write unit tests for it

--- server/utils/users.js
+++ server/utils/users.js
@@ -20,7 +20,23 @@ class Users {
         return user;
     }
 
+    removeUser(id){
+        var user = this.getUser(id);
+        if(user){
+            this.users = this.users.filter((user)=> user.id !== id);
+        }
+        return user;
+    }
 
+    getUser(id){
+        return this.users.filter((user)=>user.id===id)[0];
+    }
+
+    getUserList(room){
+        var users = this.users.filter((user) => user.room === room);
+        var namesArray = users.map((user)=> user.name);
+        return namesArray;
+    }
 }
 
 module.exports = {Users};
